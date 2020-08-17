3ab8d40b7092a1a9947d0ba225be33f7ff8baa82
Amy Shackles <amyshackles@gmail.com>
2020-08-16 17:15:24 -0700

    Updating code to be able to run locally

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 let defaultOptions = {
-  clockFaceColor: 'rgb(255, 255, 255, 0.3)',
-  secondHandPointColor: 'rgba(0, 0, 0, 0)',
-  clockOutlineColor: 'black',
-  secondHandColor: '#D40000',
-  minuteHandColor: 'black',
-  hourHandColor: 'black',
-  minuteMarksColor: 'black',
-  hourMarkColor: 'black',
-  catColor: 'yellow',
   pupilColor: 'black',
   leftScleraColor: 'white',
   rightScleraColor: 'white',
@@ -16,19 +7,51 @@ let defaultOptions = {
   scleraColor: 'white',
   scleraOutlineWidth: 2,
   scleraOutlineColor: "#000",
-  leftEarHeight: "70px",
-  leftEarColor: 'yellow',
-  rightEarColor: 'yellow',
-  headColor: 'yellow',
   leftFrontPawColor: "yellow",
   leftFrontPawSize: "50px",
   rightFrontPawColor: "yellow",
   rightFrontPawSize: "50px",
   browRidgeWidth: "200px",
   browRidgeHeight: "90px",
-  backgroundColor: 'black'
+  backgroundColor: 'black',
+  catMargin: "auto",
+  catWidth: '380px',
+  leftEarHeight: "70px",
+  clockFaceColor: 'rgb(255, 255, 255, 0.3)',
+  secondHandPointColor: 'rgba(0, 0, 0, 0)',
+  clockOutlineColor: 'black',
+  secondHandColor: '#D40000',
+  minuteHandColor: 'black',
+  hourHandColor: 'black',
+  minuteMarksColor: 'black',
+  hourMarkColor: 'black',
+  leftEarColor: 'yellow',
+  rightEarColor: 'yellow',
+  headColor: 'yellow',
+  catColor: 'yellow',
 }
 let timeZoneList = [];
+let catList = [];
+var numCats = -1;
+
+
+let animation = gsap.timeline({onComplete: animationComplete});
+animation
+.to(".ears .left-ear", { duration: 3, x: 5, y: 5, skewX: 15, scale: .9, rotation: 25}, "initial")
+.to(".ears .right-ear", { duration: 3, x: 5, y: 5, skewX: 15, scale: .9, rotation: 25}, "initial")
+.to(".mouth .right-side", { duration: 1, rotation: -20}, "initial")
+.to(".mouth .left-side", { duration: 1, rotation: 20}, "initial")
+.to(".tail", { duration: 8, rotationY: 180, rotationX: 0, y: 0, x: 220}, "initial")
+.to(".ears .left-ear", { duration: 3, x: 0, y: 0, skewY: 25, scale: .9, rotation: -5}, "second")
+.to(".ears .right-ear", { duration: 3, x: 0, y: 0, skewY: 35, scale: .9, rotation: -5}, "second")
+.to(".mouth .right-side", { duration: 1, rotation: 0}, "second")
+.to(".mouth .left-side", { duration: 1, rotation: 0}, "second")
+.to(".tail", { duration: 8, rotationY: 0, rotationX: 0, x: 0}, "second")
+
+
+function animationComplete() {
+  animation.restart();
+}
 
 function getTimeZone(timeZone) {
   if (timeZone) {
@@ -47,49 +70,56 @@ function getTimeZone(timeZone) {
   }
 }
 
-function createEye(selector) {
+function updateEye(selector) {
   var element = document.getElementById(selector);
+  console.log('element', element, 'selector', selector);
   TweenLite.set(element, {
     transformOrigin: "center"
   });
-  
+
   var bbox = element.getBBox();
   var centerX = bbox.x + bbox.width / 2;
   var centerY = bbox.y + bbox.height / 2;
-  
+
   function rotateTo(point) {
-    
+
     var dx = point.x - centerX;
     var dy = point.y - centerY;
-    
+
     var angle = Math.atan2(dy, dx);
-    
+
     TweenLite.to(element, 0.3, {
       rotation: angle + "_rad_short"
     });
   }
-  
+
   return {
     element: element,
     rotateTo: rotateTo
   };
 }
-// function styleLeftEye(cat, scleraColor, scleraOutlineWidth, scleraOutlineColor, pupilColor, pupilSize) {
-//   let leftEye = document.getElementById(`${cat}-left-eye`);
-//   styles.forEach(([key, value]) => {
-//     switch (key) {
-//       case "leftPupilColor":
-//       case "leftScleraColor":
-//         leftEye.style.fill = value;
-//         break;
-//       case "leftScleraOutlineWidth":
-//         leftEye.style.strokeWidth = value;
-//         break;
-//     }
-//   })
+function onFrame() {
+  catList.forEach(cat => {
+    let { point, leftEye, rightEye, svg, mouse } = cat;
+    point = mouse.matrixTransform(svg.getScreenCTM().inverse());
+    leftEye = leftEye.rotateTo(point);
+    rightEye = rightEye.rotateTo(point);
+  })
+    requestId = null;
+}
+window.addEventListener("mousemove", onMouseMove)
+
+function onMouseMove(event) {
+  catList.forEach((cat, index) => {
+    let { mouse, context, leftEye, rightEye } = cat;
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+  })
   
-// }
-let numCats = -1;
+  if(requestId === null) {
+    requestId = requestAnimationFrame(onFrame);
+  }
+}
 // function changeCatColor(cat, color) {
 //   let leftEar = document.getElementById(`${cat}-left-ear`), rightEar = document.getElementById(`${cat}-right-ear`)
 //   leftEar.style.borderBottomColor = color;
@@ -104,35 +134,6 @@ let numCats = -1;
 //         rightEye.style.fill = value;
 //         break;
 //     }
-//   })
-// }
-
-// function addStyles(element, styles) {
-//   styles.forEach(([key, value]) => {
-//     switch (key) {
-//       case "pupilColor":
-//       case "scleraColor":
-//         element.style.fill = value;
-//         break;
-//       case "scleraOutlineWidth":
-//         element.style.strokeWidth = value;
-//         break;
-//       case "scleraOutlineColor":
-//         element.style.stroke = value;
-//         break;
-//       case "rightEyeColor":
-//       case "leftEyeColor":
-//         element.style.borderBottomColor = value;
-//         break;
-//       case "headColor":
-//       case "leftFrontPawColor":
-//       case "rightFrontPawColor":
-//       case "leftBackPawColor":
-//       case "rightBackPawColor":
-//         element.style.backgroundColor = value;
-//         break;
-//     }
-//     element.style[key] = value;
 //   })
 // }
 
@@ -204,7 +205,7 @@ function createClock(ctx, now, options = defaultOptions) {
   ctx.save();
   ctx.lineWidth = 5;
   for (i = 0; i < 60; i++) {
-    if (i % 5!= 0) {
+    if (i % 5 != 0) {
       ctx.beginPath();
       ctx.moveTo(117, 0);
       ctx.lineTo(120, 0);
@@ -214,15 +215,15 @@ function createClock(ctx, now, options = defaultOptions) {
     ctx.rotate(Math.PI / 30);
   }
   ctx.restore();
- 
+
   var sec = now.getSeconds();
   var min = now.getMinutes();
-  var hr  = now.getHours();
+  var hr = now.getHours();
   hr = hr >= 12 ? hr - 12 : hr;
 
   // write Hours
   ctx.save();
-  ctx.rotate(hr * (Math.PI / 6) + (Math.PI / 360) * min + (Math.PI / 21600) *sec);
+  ctx.rotate(hr * (Math.PI / 6) + (Math.PI / 360) * min + (Math.PI / 21600) * sec);
   ctx.lineWidth = 14;
   ctx.beginPath();
   ctx.moveTo(-20, 0);
@@ -241,7 +242,7 @@ function createClock(ctx, now, options = defaultOptions) {
   ctx.strokeStyle = options.minuteHandColor;
   ctx.stroke();
   ctx.restore();
- 
+
   // Write seconds
   ctx.save();
   ctx.rotate(sec * Math.PI / 30);
@@ -276,14 +277,34 @@ function createClock(ctx, now, options = defaultOptions) {
 
 }
 
+function catsInit(options = defaultOptions) {
+  let cats = document.createElement('div');
+  cats.setAttribute('id', 'cats');
+  cats.setAttribute('class', 'cats');
+  cats.style.height = "100vh";
+  cats.style.width = "100vw";
+  cats.style.display = "flex";
 
-function catInit(options = defaultOptions) {
-  let idPrefix = ++numCats;
+  let svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+  svg.setAttribute("id", "svg");
+  svg.setAttribute("viewBox", "0 0 1000 1000");
+  cats.appendChild(svg);
+  document.body.appendChild(cats);
+  document.body.style.backgroundColor = options.backgroundColor
+}
+
+function catInit(options) {
+  options = {...defaultOptions, ...options}
+  ++numCats;
+  const idPrefix = numCats;
+
   let newCat = document.createElement("div");
   newCat.setAttribute("id", `cat${idPrefix}`);
   newCat.setAttribute("class", "cat");
+  newCat.style.margin = options.catMargin;
+  newCat.stylemwidth = options.catWidth;
   let ears = document.createElement("div");
-  ears.setAttribute("class", "ears")
+  ears.setAttribute("class", "ears");
   let leftEar = document.createElement("div");
   leftEar.setAttribute("id", `cat${idPrefix}-left-ear`);
   leftEar.setAttribute("class", "left-ear");
@@ -294,15 +315,17 @@ function catInit(options = defaultOptions) {
   rightEar.setAttribute("id", `cat${idPrefix}-right-ear`);
   rightEar.setAttribute("class", "right-ear");
   rightEar.style.borderBottomColor = options.catColor;
+  rightEar.style.borderBottomWidth = options.leftEarHeight;
+  rightEar.style.borderBottomStyle = "solid";
   ears.appendChild(leftEar);
   ears.appendChild(rightEar);
   newCat.appendChild(ears);
-  
+
   // Head
   let head = document.createElement('div');
   head.setAttribute("class", "head");
   head.style.backgroundColor = options.catColor;
-  
+
   // Eyes
   let eyes = document.createElement('div');
   eyes.setAttribute("class", "eyes");
@@ -310,9 +333,9 @@ function catInit(options = defaultOptions) {
   eyes.style.width = options.browRidgeWidth;
   let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   svg.setAttribute("viewBox", "0 0 120 120");
-  
+
   // Left eye
-  let leftEye = document.createElementNS("http://www.w3.org/2000/svg", "g")
+  let leftEye = document.createElementNS("http://www.w3.org/2000/svg", "g");
   leftEye.setAttribute("id", `cat${idPrefix}-left-eye`);
   let outerLeftEye = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   outerLeftEye.setAttribute("class", "eye-outer");
@@ -331,11 +354,11 @@ function catInit(options = defaultOptions) {
   leftEye.appendChild(outerLeftEye);
   leftEye.appendChild(innerLeftEye);
   svg.appendChild(leftEye);
-  
+
   // Right eye
-  let rightEye = document.createElementNS("http://www.w3.org/2000/svg", "g")
+  let rightEye = document.createElementNS("http://www.w3.org/2000/svg", "g");
   rightEye.setAttribute("id", `cat${idPrefix}-right-eye`);
-  let outerRightEye = document.createElementNS("http://www.w3.org/2000/svg", "circle", { class: "eye-inner"});
+  let outerRightEye = document.createElementNS("http://www.w3.org/2000/svg", "circle", { class: "eye-inner" });
   outerRightEye.setAttribute("class", "eye-outer");
   outerRightEye.setAttribute("cx", '95');
   outerRightEye.setAttribute("cy", "30");
@@ -354,7 +377,7 @@ function catInit(options = defaultOptions) {
   svg.appendChild(rightEye);
   eyes.appendChild(svg);
   head.appendChild(eyes);
-  
+
   // Nose
   let nose = document.createElement('div');
   nose.setAttribute("class", "nose");
@@ -371,7 +394,7 @@ function catInit(options = defaultOptions) {
   straightBit.setAttribute("class", "straight-bit");
   nose.appendChild(straightBit);
   head.appendChild(nose);
-  
+
   // Mouth
   let mouth = document.createElement("div")
   mouth.setAttribute("class", "mouth");
@@ -382,9 +405,9 @@ function catInit(options = defaultOptions) {
   mouth.appendChild(leftSide);
   mouth.appendChild(rightSide);
   head.appendChild(mouth);
-  
+
   newCat.appendChild(head);
-  
+
   // Body
   let body = document.createElement("div");
   body.setAttribute("class", "body");
@@ -408,7 +431,7 @@ function catInit(options = defaultOptions) {
   body.appendChild(torso);
   body.appendChild(rightArm);
   newCat.appendChild(body);
-  
+
   // Legs
   let legs = document.createElement("div");
   legs.setAttribute("class", "legs");
@@ -421,19 +444,18 @@ function catInit(options = defaultOptions) {
   legs.appendChild(leftLeg);
   legs.appendChild(rightLeg);
   newCat.appendChild(legs);
-  
+
   // Tail
   let tail = document.createElement("div");
   tail.setAttribute("class", "tail");
   tail.style.borderColor = options.catColor;
   newCat.appendChild(tail);
-  
+
   let cats = document.getElementById("cats");
   cats.appendChild(newCat);
-  document.body.style.backgroundColor = options.backgroundColor
   return { svg, clock, cat: newCat }
 }
-
+let requestId = null;
 
 class Cat {
   constructor(svg, clock, context, leftEye, rightEye, mouse, point, cat) {
@@ -446,33 +468,33 @@ class Cat {
     this.point = point;
     this.cat = cat;
   }
-  styleEye (style, side) {
+  styleEye(style, side) {
     let [attribute, value] = style;
     let innerEye = this[`${side}Eye`].element.querySelector('.eye-inner');
     let outerEye = this[`${side}Eye`].element.querySelector('.eye-outer');
     let partInference = /inner/gi.test(attribute) ? innerEye : outerEye;
     switch (attribute) {
-        case `${side}PupilSize`:
-          innerEye.setAttribute("r", value);
-          break;
-        case `${side}PupilColor`:
-        case `${side}EyeColor`:
-          innerEye.style.fill = value;
-          break;
-        case `${side}ScleraColor`:
-          outerEye.style.fill = value;
-          break;
-        case `${side}ScleraOutlineWidth`:
-          outerEye.style.strokeWidth = value;
-          break;
-        case `${side}ScleraOutlineColor`:
-          outerEye.style.stroke = value;
-          break;
+      case `${side}PupilSize`:
+        innerEye.setAttribute("r", value);
+        break;
+      case `${side}PupilColor`:
+      case `${side}EyeColor`:
+        innerEye.style.fill = value;
+        break;
+      case `${side}ScleraColor`:
+        outerEye.style.fill = value;
+        break;
+      case `${side}ScleraOutlineWidth`:
+        outerEye.style.strokeWidth = value;
+        break;
+      case `${side}ScleraOutlineColor`:
+        outerEye.style.stroke = value;
+        break;
       default:
         attribute = convertAttributeToStyleProp(attribute);
         partInference.style[attribute] = value;
         break;
-      }
+    }
     return this;
   }
   styleLimb(style, side, limbType) {
@@ -507,30 +529,30 @@ class Cat {
   }
   addStyles(styles) {
     console.log('styles', styles)
-        Object.entries(styles).forEach(([key, value]) => {
-          let side = /right/.test(key) ? 'right' : 'left';
-          if (/sclera|pupil|eye/gi.test(key)) {
-            this.styleEye([key, value], side);
-          }
-          if (/FrontPaw/gi.test(key)) {
-            this.styleLimb([key, value], side, 'arm');
-          } else if (/BackPaw/gi.test(key)) {
-            this.styleLimb([key, value], side, 'leg');
-          } else if (/(?:browridge)|(?:eyes)/gi.test(key)) {
-            this.styleEyes([key, value])
-          }
-        });
+    Object.entries(styles).forEach(([key, value]) => {
+      let side = /right/.test(key) ? 'right' : 'left';
+      if (/sclera|pupil|eye/gi.test(key)) {
+        this.styleEye([key, value], side);
+      }
+      if (/FrontPaw/gi.test(key)) {
+        this.styleLimb([key, value], side, 'arm');
+      } else if (/BackPaw/gi.test(key)) {
+        this.styleLimb([key, value], side, 'leg');
+      } else if (/(?:browridge)|(?:eyes)/gi.test(key)) {
+        this.styleEyes([key, value])
+      }
+    });
     return this;
   }
 }
 
 
 
-function CatFactory () {
-  this.create = function(timeZone, options) {
+function CatFactory() {
+  this.create = function (timeZone, options) {
     const { svg, clock, cat } = catInit(options);
     let context = clock.getContext("2d");
-    timeZoneList.push(timeZone);      
+    timeZoneList.push(timeZone);
     let timeZoneLabel = document.createElement("P");
     timeZoneLabel.style.color = 'white';
     timeZoneLabel.style.textAlign = "center";
@@ -541,30 +563,53 @@ function CatFactory () {
       timeZoneLabel.innerText = 'Local Time';
     }
     cat.prepend(timeZoneLabel);
-
+    console.log('cat Id', cat.id);
     let time = getTimeZone(timeZone);
     createClock(context, time, options);
     let mouse = svg.createSVGPoint();
-    let leftEye = createEye(`cat${numCats}-left-eye`);
-    let rightEye = createEye(`cat${numCats}-right-eye`);
+    let leftEye = updateEye(`${cat.id}-left-eye`);
+    let rightEye = updateEye(`${cat.id}-right-eye`);
     let point = mouse.matrixTransform(svg.getScreenCTM().inverse());
     return new Cat(svg, clock, context, leftEye, rightEye, mouse, point, cat);
   }
 }
 
-function convertAttributeToStyleProp (style) {
+function convertAttributeToStyleProp(style) {
   if (/FrontPaw/gi.test(style)) {
-    style = style.replace(/((?:left)|(?:right))FrontPaw/, '');
+    style = style.replace(/((?:left)|(?:right))FrontPaw/gi, '');
     return style[0].toLowerCase() + style.slice(1);
   } else if (/BackPaw/gi.test(style)) {
-    style = style.replace(/((?:left)|(?:right))BackPaw/, '');
+    style = style.replace(/((?:left)|(?:right))BackPaw/gi, '');
+    return style[0].toLowerCase() + style.slice(1);
+  } else if (/arm/gi.test(style)) {
+    style = style.replace(/((?:left)|(?:right))arm/gi, '');
+    return style[0].toLowerCase() + style.slice(1);
+  } else if (/leg/gi.test(style)) {
+    style = style.replace(/((?:left)|(?:right))leg/gi, '');
+    return style[0].toLowerCase() + style.slice(1);
+  } else if (/(?:eyes)|(?:browridge)/gi.test(style)) {
+    style = style.replace(/(?:eyes)|(?:browridge)/gi, '');
     return style[0].toLowerCase() + style.slice(1);
   } else if (/eye/gi.test(style)) {
-    style = style.replace(/((?:left)|(?:right))(?:I?i?nner)?(?:O?o?uter)?e?E?ye/, '');
+    style = style.replace(/((?:left)|(?:right))(?:inner)?(?:outer)?eye/gi, '');
     return style[0].toLowerCase() + style.slice(1);
   }
 }
+
+function clock() {
+  let clocks = document.querySelectorAll('.clock');
+  clocks.forEach((clock, index) => {
+    let context = clock.getContext('2d');
+    let time = getTimeZone(timeZoneList[index]);
+    createClock(context, time);
+  });
+  window.requestAnimationFrame(clock);
+}
+
+window.requestAnimationFrame(clock);
+catsInit();
 let catFactory = new CatFactory();
-let cat = catFactory.create()
-cat.addStyles({leftPupilColor: 'green', rightFrontPawBorderColor: "red", leftFrontPawBorder: "3px solid red", rightBackPawBorder: '3px solid red', leftOuterEyeOpacity: '1%'})
-console.log(cat)
+catList.push(catFactory.create())
+catList.push(catFactory.create('Australia/Sydney', {catColor: 'green'}));
+catList.push(catFactory.create('Europe/London', {catColor: 'red'}))
+catList.push(catFactory.create('Europe/Berlin', {catColor: 'rebeccapurple'}))
