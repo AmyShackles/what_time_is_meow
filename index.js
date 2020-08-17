59aca968cea218a8e187eb900caeb15e626b09fe
Amy Shackles <amyshackles@gmail.com>
2020-08-17 13:32:21 -0700

    Moving animation to separate js file to ensure it loads after cat initialization

--- index.js
+++ index.js
@@ -426,24 +426,3 @@ catList.push(berlinCat);
 
 catList[2].addStyles({leftEyeColor: 'blue', minuteMarkColor: 'grey', headColor: 'fuchsia',});
 catList[1].addStyles({leftBackPawColor: "red", secondHandColor: "white", clockFaceColor: "rgb(23, 25, 22, 0.3)"});
-
-
-
-// Animation needs to be at bottom of file to ensure cats have been created beforehand
-let animation = gsap.timeline({onComplete: animationComplete});
-animation
-.to(".ears .left-ear", { duration: 3, x: 5, y: 5, skewX: 15, scale: .9, rotation: 25}, "initial")
-.to(".ears .right-ear", { duration: 3, x: 5, y: 5, skewX: 15, scale: .9, rotation: 25}, "initial")
-.to(".mouth .right-side", { duration: 1, rotation: -20}, "initial")
-.to(".mouth .left-side", { duration: 1, rotation: 20}, "initial")
-.to(".tail", { duration: 8, rotationY: 180, rotationX: 0, y: 0, x: 220}, "initial")
-.to(".ears .left-ear", { duration: 3, x: 0, y: 0, skewY: 25, scale: .9, rotation: -5}, "second")
-.to(".ears .right-ear", { duration: 3, x: 0, y: 0, skewY: 35, scale: .9, rotation: -5}, "second")
-.to(".mouth .right-side", { duration: 1, rotation: 0}, "second")
-.to(".mouth .left-side", { duration: 1, rotation: 0}, "second")
-.to(".tail", { duration: 8, rotationY: 0, rotationX: 0, x: 0}, "second")
-
-
-function animationComplete() {
-  animation.restart();
-}
